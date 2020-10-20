c3a7b4ee4f2b46510729187104682d693a67bb68
Ferre Hamblok <ferre@hamblok.be>
2020-10-20 23:03:18 +0200

    Added homepage layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,27 @@
 module.exports = (isProd) => ({
-    prefix: '',
-    future: {
-      removeDeprecatedGapUtilities: true,
-      purgeLayersByDefault: true
-    },
-    purge: {
-      enabled: isProd,
-      content: ['**/*.html', '**/*.ts']
-    },
-    theme: {
-      extend: {
-        colors: {
-          dark: '#2C3035',
-          primary: '#3EC7FF',
-          secondary: '#0F5B69'
-        }
+  prefix: '',
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true
+  },
+  purge: {
+    enabled: isProd,
+    content: ['**/*.html', '**/*.ts']
+  },
+  theme: {
+    extend: {
+      colors: {
+        dark: '#2C3035',
+        primary: '#3EC7FF',
+        secondary: '#0F5B69'
+      },
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+        '108': '27rem',
+        '120': '30rem',
       }
     }
+  }
 });
