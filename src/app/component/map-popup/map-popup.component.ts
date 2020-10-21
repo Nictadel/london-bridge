8813964e93e88deaf8d3618bf8201ecbec7b58eb
Ferre Hamblok <ferre@hamblok.be>
2020-10-21 06:49:13 +0200

    Map popup designs

--- src/app/component/map-popup/map-popup.component.ts
+++ src/app/component/map-popup/map-popup.component.ts
@@ -10,9 +10,24 @@ export class MapPopupComponent implements OnInit {
 
   @Input() connection: Connection;
 
+  public stateData: any;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    let stateData = {};
+    this.connection.connection_ends.forEach(end => {
+      let shoreName = end.shore.short_name.toLowerCase();
+      end.services.forEach(service => {
+        if(!stateData[service.type.name]){
+          stateData[service.type.name] = {};
+        }
+        stateData[service.type.name][shoreName] = service.state.name;
+      })
+    })
+    this.stateData = stateData;
+
   }
 
 }
