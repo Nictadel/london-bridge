f179da3ea73b0a0a1d9277baaa1c030801274abc
Ferre Hamblok <ferre@hamblok.be>
2020-10-23 16:19:04 +0200

    Fix faulty asset url in production environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   base_url: 'https://awv-fietsverbindingen.development.appwi.se/api',
-  asset_url: 'https://awv-fietsverbindingen.development.appwi.se/media',
+  asset_url: 'https://awv-fietsverbindingen.development.appwi.se',
 
   mapbox_token: 'pk.eyJ1IjoibmljdGFkZWwiLCJhIjoiY2tnZ2lienl1MXJlODJycWEydXkxN3prdyJ9.PZ9EPGZtKuHOyAeCGJUBSA',
   mapbox_styleURL: 'mapbox://styles/nictadel/ckggf8cxd0dxl19moeliw37i1'
